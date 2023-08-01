638c970f33882bf189be162e76092e80ee59338f
lazydev01 <arpeetbhavsar01@gmail.com>
2023-08-01 22:19:22 +0530

    Implemented JWT Helper Class, JWTAuthenticatioNEntryPoint and JWTAuthenticationFilter

--- src/main/java/com/spring/security/jwt/authentication/config/MySecurityConfig.java
+++ src/main/java/com/spring/security/jwt/authentication/config/MySecurityConfig.java
@@ -3,13 +3,25 @@ package com.spring.security.jwt.authentication.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class MySecurityConfig {
     @Bean
     public UserDetailsService userDetailsService(){
+        UserDetails user = User.builder().username("arpit").password(passwordEncoder().encode("arpit")).roles("ADMIN").build();
+        UserDetails user2 = User.builder().username("banz").password(passwordEncoder().encode("banz")).roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user, user2);
+    }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
     }
 }
--- src/main/java/com/spring/security/jwt/authentication/controllers/HomeController.java
+++ src/main/java/com/spring/security/jwt/authentication/controllers/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -19,5 +20,10 @@ public class HomeController {
     public List<User> getUser(){
         System.out.println("Getting Users");
         return this.userService.getUsers();
+    }
+
+    @GetMapping("/current-user")
+    public Principal getCurrentUser(Principal principal){
+        return principal;
     }
 }
